745e4e26883a496d51132c3051c898c6d2970ec6
Alim Weber <alim.weber@outlook.com>
2023-12-07 21:54:53 +0100

    cors

--- index.js
+++ index.js
@@ -7,12 +7,19 @@ const server = http.createServer(app)
 
 const io = new Server(server)
 
+const cors = require("cors");
 
 const PORT = process.env.PORT || 5000
 
+
+app.use(cors())
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "static")))
-
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE")
+	next()
+})
 
 
 app.post("login", (req, res) => {
